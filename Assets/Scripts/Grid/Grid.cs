0c5dd345877d8d5c2a291f7c17909247b147850f
matiasbian <matiasezequielbian@gmail.com>
2022-12-23 17:50:57 -0300

    First commit

---
+++ Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    int height;
+    int width;
+    public Grid (int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+}
